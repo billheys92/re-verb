1e98f840af4040452f46d8b13757361a19be3610
Bill <bill_heys@hotmail.com>
2014-07-01 18:05:47 -0230

    bins

--- Reverb/gen/com/re/reverb/R.java
+++ Reverb/gen/com/re/reverb/R.java
@@ -1091,21 +1091,31 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int buttonMainFeed=0x7f050041;
+        public static final int buttonRegions=0x7f050044;
+        public static final int buttonUserProfile=0x7f050047;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int edit_message=0x7f05003d;
+        public static final int editTextMainFeed=0x7f050040;
+        public static final int editTextRegions=0x7f050043;
+        public static final int editTextUserProfile=0x7f050046;
+        public static final int edit_message=0x7f05003e;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int fragmentContainer=0x7f05003c;
+        public static final int fragmentMainFeed=0x7f05003f;
+        public static final int fragmentRegions=0x7f050042;
+        public static final int fragmentUserProfile=0x7f050045;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1113,11 +1123,11 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int mainViewPager=0x7f05003d;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int pager=0x7f05003c;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1195,10 +1205,12 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int fragment_find_regions=0x7f030019;
+        public static final int activity_main_view_pager=0x7f030019;
         public static final int fragment_main=0x7f03001a;
-        public static final int fragment_user_profile=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int fragment_main_feed=0x7f03001b;
+        public static final int fragment_regions=0x7f03001c;
+        public static final int fragment_user_profile=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1247,9 +1259,11 @@ containing a value of this type.
         public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
         public static final int button_send=0x7f0a000f;
-        public static final int edit_message=0x7f0a000e;
+        public static final int edit_main_feed_message=0x7f0a000e;
         public static final int edit_search_regions_message=0x7f0a0013;
         public static final int edit_username_message=0x7f0a0012;
+        public static final int swipe_left=0x7f0a0015;
+        public static final int swipe_right=0x7f0a0014;
         public static final int title_activity_main=0x7f0a0011;
     }
     public static final class style {
