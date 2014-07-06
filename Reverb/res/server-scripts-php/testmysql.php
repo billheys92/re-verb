ce3e68e4f274fa209688fe6b6e5f5a65e170f678
jacobbrown0192 <jacobbrown0192@gmail.com>
2014-07-06 19:00:23 -0230

    .... Annoying had to remove as there is no way to factor the php scripts so the android app ignores them

--- Reverb/res/server-scripts-php/testmysql.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$link = mysqli_connect('localhost', 'root', '', 'trying');
-if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
-} 
-echo 'Connection OK'; mysqli_close($link); 
-?> 
--- Reverb/res/server-scripts-php/variabletest.php
+++
@@ -1,4 +0,0 @@
-<?php 
-// The value of the variable name is found
-echo "<h1>Hello " . $_GET["name"] . "</h1>";
-?> 
