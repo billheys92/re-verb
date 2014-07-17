4b21ff7677e20f7613e19c7f5234dd1f6d40e8d1
Bill <bill_heys@hotmail.com>
2014-07-17 17:33:05 -0230

    Revert "Merge pull request #10 from billheys92/LocationData"
    This reverts commit 77d0e9a9fce3c5a02329a1a013fde814a4af848d, reversing
    changes made to 5625b5b0e1c7585b791ff12bf99450614cbd1693.

--- Reverb/src/com/re/reverb/ui/MainViewPagerActivity.java
+++ Reverb/src/com/re/reverb/ui/MainViewPagerActivity.java
@@ -1,10 +1,5 @@
 package com.re.reverb.ui;
 
-import android.content.Context;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -15,13 +10,8 @@ import android.view.View;
 
 import com.re.reverb.R;
 
-public class MainViewPagerActivity extends FragmentActivity implements LocationListener
+public class MainViewPagerActivity extends FragmentActivity
 {
-	
-	private String latitude;
-	private String longitude;
-	private LocationManager locationManager;
-	private String provider;	
 	
 	static final int NUM_PAGES = 3;
 	private int currentPage = 1;
@@ -39,27 +29,6 @@ public class MainViewPagerActivity extends FragmentActivity implements LocationL
         mViewPager = (ViewPager) findViewById(R.id.mainViewPager);
         mViewPager.setAdapter(mPagerAdapter);
         mViewPager.setCurrentItem(currentPage);
-        // Get the location manager
-	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    // Define the criteria how to select the location provider -> use
-	    // default
-	    Criteria criteria = new Criteria();
-	    provider = locationManager.getBestProvider(criteria, false);
-	    Location location = locationManager.getLastKnownLocation(provider);
-
-	    // Initialize the location fields
-	    if (location != null) 
-	    {
-	      System.out.println("Provider " + provider + " has been selected.");
-	      onLocationChanged(location);
-	    } 
-	    else 
-	    {
-	      latitude = "Location not available";
-	      longitude = "Location not available";
-	      System.out.println(latitude);
-	      System.out.println(longitude);
-	    }
     }
     
     public class MainViewPagerAdapter extends FragmentPagerAdapter {
@@ -90,29 +59,5 @@ public class MainViewPagerActivity extends FragmentActivity implements LocationL
     	System.out.println("Message Sent");
     }
     
-	@Override
-	public void onLocationChanged(Location location) {
-	    double lat = (double) (location.getLatitude());
-	    double lng = (double) (location.getLongitude());
-	    latitude = String.valueOf(lat);
-	    longitude = String.valueOf(lng);
-	    System.out.println("Latitute:" + latitude);
-	    System.out.println("Longitute:" + longitude);
-	}
-	
-	@Override
-	public void onProviderEnabled(String provider) {
-		System.out.println("Enabled new provider " + provider);
-	}
-	
-	@Override
-	public void onProviderDisabled(String provider) {
-		System.out.println("Disabled provider " + provider);
-	}
-	
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	}
+    
 }
