a6e25840a6d9bdeaa10d89b8f17705e2f20df529
Bill <bill_heys@hotmail.com>
2014-07-18 11:23:04 -0230

    Fixed an error where the whats up text view was hidden

---
+++ Reverb/src/com/re/reverb/androidBackend/errorHandling/UnsuccessfulFetchPostsException.java
@@ -0,0 +1,10 @@
+package com.re.reverb.androidBackend.errorHandling;
+
+public class UnsuccessfulFetchPostsException extends Exception
+{
+
+	public UnsuccessfulFetchPostsException(String message){
+		super(message);
+	}
+
+}
