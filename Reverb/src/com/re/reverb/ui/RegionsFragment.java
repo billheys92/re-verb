fe9e9d16175375f9e8554a106d0c7b94ecca8b98
Bill <bill_heys@hotmail.com>
2015-01-14 11:25:23 -0330

    committing files that are causing problems with pulling

--- Reverb/src/com/re/reverb/ui/RegionsFragment.java
+++ Reverb/src/com/re/reverb/ui/RegionsFragment.java
@@ -1,13 +1,6 @@
 package com.re.reverb.ui;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
 
 import com.re.reverb.R;
 
