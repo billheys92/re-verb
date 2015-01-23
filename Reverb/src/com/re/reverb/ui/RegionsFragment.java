eb38eb0ceae2e6d5de1818bd19eb69315c62cab9
Bill <bill_heys@hotmail.com>
2015-01-23 18:39:59 -0330

    Almost forgot to put in these changes...

--- Reverb/src/com/re/reverb/ui/RegionsFragment.java
+++ Reverb/src/com/re/reverb/ui/RegionsFragment.java
@@ -1,18 +1,22 @@
 package com.re.reverb.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.re.reverb.R;
 import com.re.reverb.androidBackend.AvailableRegionsUpdateRegion;
 import com.re.reverb.androidBackend.Reverb;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class RegionsFragment extends ListFragment implements AvailableRegionsUpdateRegion
 {
@@ -44,7 +48,7 @@ public class RegionsFragment extends ListFragment implements AvailableRegionsUpd
         }
         try {
             view = inflater.inflate(R.layout.fragment_regions, container, false);
-            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, Reverb.getInstance().getRegionManager().getNearbyRegionNames());
+            adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, nearbyRegionNames);
             setListAdapter(adapter);
         } catch (InflateException e) {
             /* fragment is already there, just return view as it is */
@@ -57,8 +61,21 @@ public class RegionsFragment extends ListFragment implements AvailableRegionsUpd
     @Override
     public void onAvailableRegionsUpdated()
     {
-//        this.nearbyRegionNames = Reverb.getInstance().getRegionManager().getNearbyRegionNames();
+        this.nearbyRegionNames.clear();
+        for(String name: Reverb.getInstance().getRegionManager().getNearbyRegionNames()) {
+            this.nearbyRegionNames.add(name);
+        }
+//        Collections.copy(this.nearbyRegionNames, Reverb.getInstance().getRegionManager().getNearbyRegionNames());
 //        this.subscribedRegionNames = Reverb.getInstance().getRegionManager().getSubscribedRegionNames();
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onListItemClick(ListView parent, View v, int position, long id)
+    {
+        super.onListItemClick(parent, v, position, id);
+        Intent intent = new Intent(this.getActivity(), CreateRegionActivity.class);
+        intent.putExtra("SELECTED_REGION_ID", position);
+        startActivity(intent);
     }
 }
